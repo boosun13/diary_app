2c276127cffbb1455c438fd17a83301b84503ec8
boosun13 <double0two22@gmail.com>
2020-06-22 00:00:52 +0900

    display styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :index
+
+    # GET /blogs
+    # GET /blogs.json
+    def index
+        @blogs = Blog.all.order(id: "DESC")
+    end
 end
