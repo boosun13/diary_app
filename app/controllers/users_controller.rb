0c85beeb24aa03a84a98687cdd9d86a0cc2ff17f
boosun13 <double0two22@gmail.com>
2020-06-29 23:46:30 +0900

    login logout ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
 class UsersController < ApplicationController
-
+  
   def index
     @users = User.all.
     paginate(page: params[:page], per_page: 10)
   end
-
+  
   def show
     @user = User.find_by(id: params[:id])
   end
-
+  
   def new
     @user = User.new
   end
-
+  
   def create
     @user = User.new(name: params[:name],
       email: params[:email],
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
     @user = User.find_by(id: [params[:id]])
     @user.name = params[:name]
     @user.email = params[:email]
-
+    
     if params[:image]
       if params[:image] == "reset"
         @user.image_name = "noimage.png"
@@ -45,8 +45,8 @@ class UsersController < ApplicationController
         File.open("public/user_images/#{@user.image_name}", 'wb') { |f| f.write(image.read) }
       end
     end
-
-
+    
+    
     if @user.save
       redirect_to ("/users"), notice: 'User was successfully updated.'
     else
@@ -62,4 +62,26 @@ class UsersController < ApplicationController
       format.html { redirect_to "/users", notice: 'Blog was successfully destroyed.' }
     end
   end
+  
+  def login_form
+  end
+  
+  def login
+    @user = User.find_by(email: params[:email],
+      password: params[:password])
+    if @user
+      session[:user_id] =@user.id
+      redirect_to ("/blogs"), notice: 'Login succeeded. Hello World !'
+    else
+      @error_message = "Email address or password is error"
+      render ("/users/login_form")
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to ("/login"), notice: 'Logout succeeded. See you !'
+  end
+
+
 end
